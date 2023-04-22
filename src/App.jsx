f2b51fa60593df068270b96f99377c102626c78b
nathalia <nathaliatroina@gmail.com>
2023-04-22 11:28:55 -0400

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { CiCoffeeCup, CiLocationOn } from 'react-icons/ci'
+import {GiShoppingCart} from 'react-icons/gi'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
   
-      <h1>Coffee Delivery</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className='headerLogo'>
+       <CiCoffeeCup />
+       <span>Coffee Delivery</span>
+        </div>
+        <div className='headerLocation'>
+          < CiLocationOn />
+          <span>New York</span>
+        </div>
+        <div className='headerCart'>
+          <GiShoppingCart/>
+          <span>0</span>
+        </div>
+      </header>
+      <main>
+        main section
+      </main>
+      <footer>
+      it is a footer
+      </footer>
     </>
   )
 }
